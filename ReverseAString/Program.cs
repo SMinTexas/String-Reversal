c72ab453716ed99b79ec09a4a6a29e3ac6da6ff1
Steven Murray <32107837+SMinTexas@users.noreply.github.com>
2019-03-26 20:22:41 -0500

    Add files via upload

---
+++ ReverseAString/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReverseAString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Write a phrase or sentence to reverse: ");
+            String s = Console.ReadLine();
+
+            if (s.Length == 0)
+                Console.WriteLine("You must enter a string to proceed.");
+            else
+            {
+                char[] arr = s.ToCharArray();
+                Array.Reverse(arr);
+                Console.WriteLine(arr);
+            }
+
+            Console.Read();
+        }
+    }
+}
